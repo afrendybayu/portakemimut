97e630e932f353a3a1a9aa076f09bbb7e533eecf
afrendybayu <afrendy@gmail.com>
2014-05-14 11:15:40 +0700

    ada controller excelgid

---
+++ app/controller/ExcelGrid.js
@@ -0,0 +1,63 @@
+Ext.define('rcm.controller.ExcelGrid', {
+    extend: 'Ext.app.Controller',
+
+    views: [
+		'rcm.view.Util',
+		'dataentry.ExcelGrid'
+    ],
+
+    controllers: [
+
+    ],
+
+    stores: [
+		'Hirarki',
+		'RunningHour'
+    ],
+    
+    models: [
+		'RunningHour'
+    ],
+    
+    /*
+    init: function() {
+		var me = this;
+        me.control({
+			'excelgrid': {
+				recordedit: me.updateGrid,
+				//EqClick: me.equipClick
+				//specialkey: me.handleSpecialKey,
+			},
+			
+		});
+    },
+    
+    updateGrid: function(view, e) {
+        var me=this, tv=e.value; drow=this.getRunningHourStore().getAt(e.rowIdx);
+        alert(",muncul dulu");
+        //var at=e.row;
+        //rcmSettings.asa = this.getRunningHourStore().getAt(e.rowIdx).data;
+        //var alertText = ' ';   for (property in e) { alertText += property + ':' + e[property]+'; ';   }
+		//console.log("updateGrid isi: "+tv);
+
+		if ((tv=='')||(tv==e.originalValue))	{		//||
+			//console.log("updateGrid numpang lewat");
+		} else if (tv==e.originalValue)	{
+			//console.log("update nilai, sudah beda");
+		} else if ((tv==24)||(tv=="24:00"))	{
+			//this.simpanRH(e);
+		} else {
+			//this.buildFormGagal(e);
+		}
+	},
+    //*/
+    ubahFieldRH: function()	{
+		this.getRunningHourModel().setFields(rcm.view.Util.Ublntgl());
+	},
+    
+    onLaunch: function() {
+		this.ubahFieldRH();
+        alert("ini muncul: onLaunch");
+	}
+
+});
