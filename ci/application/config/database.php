53e57caf75ee2155e7ff7efdf214a2a844ea9db6
afrendybayu <afrendy@gmail.com>
2014-06-03 05:47:18 +0700

    ganti konfig database

--- ci/application/config/database.php
+++ ci/application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'tentakel';
+$db['default']['username'] = 'monita';
+$db['default']['password'] = 'monita2011';
 $db['default']['database'] = 'medco';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
