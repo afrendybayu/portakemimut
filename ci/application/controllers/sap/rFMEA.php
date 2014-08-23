af9a798cdb58bcc422e7fac945997de222926d8e
afrendybayu <afrendy@gmail.com>
2014-08-23 11:51:52 +0700

    php sap rFMEA

---
+++ ci/application/controllers/sap/rFMEA.php
@@ -0,0 +1,106 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class rFMEA extends CI_Controller {
+	
+	public function sapCause()	{
+
+		try {
+			$this->load->model('sap');
+			
+			$hsl = $this->sap->get_jmlWO();
+			//print_r($hsl);
+			
+			$sap = array();
+			foreach ($hsl as $row) {
+				//print_r($row);
+				//echo $row->kode;
+				//*
+				if (strcmp($row->kode,"EP01")==0)	{
+					$row->nama = 'EP01 Corrective Order';
+					$row->color = '#2f7ed8';
+				}
+				if (strcmp($row->kode,"EP02")==0)	{
+					$row->nama = 'EP02 Breakdown Order';
+					$row->color = '#0d233a';
+				}
+				if (strcmp($row->kode,"EP03")==0)	{
+					$row->nama = 'EP03 Scheduled Order';
+					$row->color = '#8bbc21';
+				}
+				if (strcmp($row->kode,"EP04")==0)	{
+					$row->nama = 'EP04 General Order';
+					$row->color = '#910000';
+				}
+				if (strcmp($row->kode,"EP05")==0)	{
+					$row->nama = 'EP05 Modification Order';
+					$row->color = '#ffa81f';
+				}
+				
+				//if (strcmp($row['kode'],"EP05")!=0)	{
+					$sap[] = $row;
+				//*/
+			}
+			
+			$jsonResult = array(
+				'success' => true,
+				'sap' => $sap
+			);
+			
+			//print_r($arAR);
+		}
+		catch (Exception $e){
+			$jsonResult = array(
+				'success' => false,
+				'message' => $e->getMessage()
+			);	
+		}
+		
+		echo json_encode($jsonResult);
+	
+	}
+	
+	public function getCause()	{
+		try {
+			$group = $this->input->get('gr')?:'0';
+			$this->load->model('sap');
+			//$hsl = $this->sap->get_cause();
+			
+			//echo "group: $group, jml: ".count($hsl)."<br/>";
+			//print_r($hsl); echo "<br/>";
+			
+			$jsonResult = array(
+				'success' => true,
+				'sapcause' => $this->sap->get_cause()
+			);
+		}
+		catch (Exception $e){
+			$jsonResult = array(
+				'success' => false,
+				'message' => $e->getMessage()
+			);	
+		}
+		echo json_encode($jsonResult);
+	}
+	
+	public function getCauseInfo()	{
+		try {
+			$cause = $this->input->get('cause')?:'';
+			$this->load->model('sap');
+
+			$jsonResult = array(
+				'success' => true,
+				'sapcause' => $this->sap->get_cause_info($cause)
+			);
+		}
+		catch (Exception $e){
+			$jsonResult = array(
+				'success' => false,
+				'message' => $e->getMessage()
+			);	
+		}
+		echo json_encode($jsonResult);
+	}
+}
+
+/* End of file rFMEA.php */
+/* Location: ./application/controllers/rFMEA.php */
