8b574e7d0518c5c892c4b9e023d2b8cf1355ec9c
Jono <jono@root>
2014-11-12 11:07:09 +0700

    ubah opartdef store

--- app/store/OpartDefs.js
+++ app/store/OpartDefs.js
@@ -2,26 +2,7 @@
 Ext.define('rcm.store.OPartDefs', {
     extend: 'Ext.data.Store',
     model: 'rcm.model.OPartDef' ,
-    // requires:[
-		// 'Ext.data.proxy.Ajax'
-		// ,'rcm.model.Aksi'
-	// ],
-	
-	
-	
+    
 	//autoLoad: true,
-	
-	/*proxy: {
-		type: 'ajax',
-		api: {
-			read: 'ci/index.php/rh/rOPart/rOpartDef'
-        },
-        reader: {
-            type: 'json',
-            root: 'opartdef',
-            messageProperty: 'message'
-        }
-    }*/
-	
 	
 });
