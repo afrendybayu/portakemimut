ad842624164e754b0ee51c056f22ce51ea9210b1
jono <jono.daunbiru@gmail.com>
2014-11-14 13:13:08 +0700

    gt pmdef

--- app/view/konfig/PmDef.js
+++ app/view/konfig/PmDef.js
@@ -8,32 +8,22 @@ Ext.define('rcm.view.konfig.PmDef', {
         
     ],
 	
-	layout	: {
-		type : 'border'
-		
-	},
+    layout	: 'border',
 	defaults: {autoScroll: true},
 	
 	items : [{
 		title	: 'Form Predictive Maintenance',
-        region	: 'center',     
-        xtype	: 'panel',
-		height	: '40%',
-		layout	: 'fit',
-		autoScroll: true,
-		frame	: true,
-		items	: [{
-			xtype 	: 'f_PmDef',
-			
-		}]
-		
+        region	: 'east',     
+        xtype	: 'f_PmDef',
+		minWidth: '420',
+		collapsible	: true,
+		split	: true
+
 	},{
 		title	: 'Data Predictive Maintenance',
-        region	: 'south',     
+        region	: 'center',     
         xtype	: 'gridPmDef',
-        height	: '60%',
-        collapsible	: true,
-		split	: true,
+	
 	}]
 	
 	
