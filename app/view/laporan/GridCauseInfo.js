3ba410a9d986fa40c44c3764f0e3fa50090c7ecc
afrendybayu <afrendy@gmail.com>
2015-01-27 17:55:44 +0700

    filter di grid untuk cause damage, objectpart

--- app/view/laporan/GridCauseInfo.js
+++ app/view/laporan/GridCauseInfo.js
@@ -6,6 +6,7 @@ Ext.define('rcm.view.laporan.GridCauseInfo', {
 	loadMask: true,
     
     dstore: '-',
+    features: [{ftype: 'filters',local: true }],
 	//store: 'SapCauseInfo',
 	
 	initComponent: function() {
@@ -16,15 +17,15 @@ Ext.define('rcm.view.laporan.GridCauseInfo', {
 			{ xtype:'rownumberer',width:50 },
 			{ header:'No PID',dataIndex:'noorder',width:75 },
 			{ header:'No Order',dataIndex:'nosap',width:75 },
-			{ header:'Tipe',dataIndex:'tipe',width:40 },
-			{ header:'Equipment',dataIndex:'equip',flex:1 },
+			{ header:'Tipe',dataIndex:'tipe',width:40, filter: { type: 'string' } },
+			{ header:'Equipment',dataIndex:'equip',flex:1, filter: { type: 'string' } },
 			{ header:'Object Part',dataIndex:'opartnm',flex:1 },
 			{ header:'Plan Start',dataIndex:'plnstr',flex:1 },
 			{ header:'Description Order',dataIndex:'orderdesc', minWidth: 250,flex:1 },
-			{ header:'Damage',dataIndex:'damagenm',width:120 },
-			{ header:'Cause',dataIndex:'causenm',width:180 },
+			{ header:'Damage',dataIndex:'damagenm',width:120, filter: { type: 'string' } },
+			{ header:'Cause',dataIndex:'causenm',width:180, filter: { type: 'string' } },
 			{ header:'Down Time',dataIndex:'down',width:80 },
-			{ header:'Main Work',dataIndex:'mainwork',width:75 },
+			{ header:'Main Work',dataIndex:'mainwork',width:75, filter: { type: 'string' } },
 			{ header:'Biaya ($)',dataIndex:'biaya',width:75,renderer:'usMoney' }
 		]};
 		me.callParent(arguments);
